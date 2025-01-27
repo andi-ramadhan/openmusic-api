388fd132ca7d5374ec218334bfff827a277ec283
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-01-27 18:35:08 +0700

    Foreign Key added for playlist-users and songs-albumId

---
+++ migrations/1737881212016_playlist.js
@@ -0,0 +1,39 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('playlists', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('playlist', 'fk_playlist.owner_users_data.id', 'FOREIGN KEY(owner) REFERENCES users_data(id) ON DELETE CASCADE');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropConstraint('playlist', 'fk_playlist.owner_users_data.id');
+
+  pgm.dropTable('playlist');
+};
