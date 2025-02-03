9543c89d73fe1b069c3b42aec93613e50dbe2825
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-02-03 23:23:30 +0700

    Fetch migrations files from v1

---
+++ migrations/1736853697470_create-table-songs-data.js
@@ -0,0 +1,49 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('songs_data', {
+    id: {
+      type: 'VARCHAR(21)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INT',
+    },
+    albumId: {
+      type: 'VARCHAR(24)',
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('songs_data');
+};
